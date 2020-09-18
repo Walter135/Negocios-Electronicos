3052b846111b0bad33de043a2cf94cbdffe95c15
Nestor Quesada <nquesadach@gmail.com>
2020-09-17 20:40:00 -0500

    agregando productos

--- includes/templates/productos.php
+++ includes/templates/productos.php
@@ -1,195 +1,59 @@
 <?php 
           try {
               require_once('includes/funciones/bd_conexion.php');
-              $sql="SELECT * from invitados";
+              $sql="SELECT * from categoria";
               $resultado=$conn->query($sql);
+              $num_categorias=$resultado->num_rows;
           } catch (Exception $e) {
             echo $e->getMessage();
           }
 
        ?>
-       <div class="contenedor">
-           <nav class="navegacion-principal menu-programa clearfix secundaria">
-            <a href="#memorias">Memorias</a>
-            <a href="#discosduro">Discos Duros</a>
-            <a href="#placasmadre">Placas Madre</a>
-            <a href="#procesadores">Procesadores</a>
-            <a href="#tarjetasvideo">Tarjetas de Video</a>
-            <a href="#case">Case</a>
-            <a href="#fuentespoder">Fuentes de Poder</a>
-            <a href="#laptops">Laptops</a> 
-          </nav>
-    
-          <div class="galeria ocultar info-producto" id= "memorias">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
 
-          <div class="galeria ocultar" id= "discosduro">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+      <div class="contenedor">
+        <nav class="navegacion-principal menu-programa clearfix secundaria">
+          <?php while ($categorias=$resultado->fetch_assoc()) { ?>
+            <a href="#categoria<?php echo $categorias["ID_CATEGORIA"] ?>"><?php echo $categorias["DESCRIPCION"] ?></a>
+          <?php } ?>
+        </nav>
 
-          <div class="galeria ocultar" id= "placasmadre">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+        <?php
+          for($indiceCat = 1; $indiceCat <= $num_categorias; $indiceCat++){?>
+            <div class="galeria ocultar info-producto" id= "categoria<?php echo $indiceCat ?>">
+              <?php 
+                try {
+                    $sqlProductos="SELECT * from producto where id_categoria = $indiceCat ";
+                    $resultadoProductos=$conn->query($sqlProductos);
+                } catch (Exception $e) {
+                  echo $e->getMessage();
+                }
+              ?>
 
-          <div class="galeria ocultar" id= "procesadores">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+              <section class="invitados contenedor seccion">
 
-          <div class="galeria ocultar" id= "tarjetasvideo">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+                <ul class="lista-productos clearfix">
+                    <?php while ($productoxCategoria=$resultadoProductos->fetch_assoc()) { ?>
+                        <li>
+                          <div class="producto">
+                            <a class="producto-info">
+                            <img src="img/<?php echo $productoxCategoria["IMAGEN"] ?>.jpg" alt="imagen producto">
+                            <p><?php echo $productoxCategoria["DESC_REDUCIDA"] ?> Precio: S/ <?php echo $productoxCategoria["PRECIO"]?></p>
+                            </a>
+                          </div>
+                        </li>
+                <?php } ?>
+                </ul>
 
-          <div class="galeria ocultar" id= "case">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+              </section>
 
-          <div class="galeria ocultar" id= "fuentespoder">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+            </div>          
+        <?php  
+        }
+        ?>
 
-          <div class="galeria ocultar" id= "laptops">
-            <a href="img/placasmadre/Asrock.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Asrock.jpg">
-            </a>
-            <a href="img/placasmadre/DataOne.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/DataOne.jpg">
-            </a>
-            <a href="img/placasmadre/Gigabyte.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Gigabyte.jpg">
-            </a>
-            <a href="img/placasmadre/Sapphire.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Sapphire.jpg">
-            </a>
-            <a href="img/placasmadre/Seasonic.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Seasonic.jpg">
-            </a>
-            <a href="img/placasmadre/Thermaltake.jpg" data-lightbox="galeria">
-            <img src="img/placasmadre/thumbs/Thermaltake.jpg">
-            </a>
-          </div>
+      </div>  
 
-        </div>
-      <div style="clear:both"></div>
+       <div style="clear:both"></div>
        <?php 
        $conn->close() 
        ?>
