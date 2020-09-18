29aa472b8ec4da0eaa0b354acfa9daa86732dd77
Nestor Quesada <nquesadach@gmail.com>
2020-09-17 22:06:09 -0500

    mas productos

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,6 @@
 <?php 
-	$conn= new mysqli('localhost:3307','root','root','gdlwebcamp');
+	/*$conn= new mysqli('localhost:3307','root','root','gdlwebcamp');*/
+	$conn= new mysqli('localhost:3307','root','T3chsyscom','gdlwebcamp');
 	if($conn->connect_error){
 		echo $error=$conn->connect_error;
 	}
